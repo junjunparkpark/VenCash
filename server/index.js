6909a99a20548548a1c65c16d1dc95dfaeb66d77
Jun Park <jun.park@live.com>
2017-10-26 22:22:09 -0700

    Initialize app.js to handle initial routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 'use strict';
-const app = require('./app');
-const db = require('../db');
+const express = require('express');
+const app = express();
 const PORT = process.env.port || 3000;
+
+const http = require('http').createServer(app);
 
 app.listen(PORT, () => {
   console.log('Server listening on port 3000!');
