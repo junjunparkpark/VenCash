54c1d77dcff31d498b144f395fb5aacbec0be8ae
Jun Park <jun.park@live.com>
2017-10-26 22:06:18 -0700

    Configure Webpack to bundle in a Public/Dist directory

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const config = {
+  entry: './client/src/app',
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: ['node_modules'],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
+
+export default config;
